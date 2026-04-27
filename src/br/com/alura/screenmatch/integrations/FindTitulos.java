eec2301e1aa7ed39b13cb2a310ebbdfa0eaaf5ad
Wellington <44244994+wellingtonolive@users.noreply.github.com>
2026-04-27 00:21:05 -0300

    feature: ajustes

--- src/br/com/alura/screenmatch/integrations/FindTitulos.java
+++ src/br/com/alura/screenmatch/integrations/FindTitulos.java
@@ -8,7 +8,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
 
 public class FindTitulos {
 
